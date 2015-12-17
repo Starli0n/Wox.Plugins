54bf09b7fe040aa50ba1770ff714bde216616d27
Starli0n <Starli0n@users.noreply.github.com>
2015-12-17 14:07:09 +0100

    Initial Commit

---
+++ HelloWorld/Main.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Wox.Plugin;
+using System.Windows.Forms;
+
+namespace HelloWorld
+{
+    public class Main : IPlugin
+    {
+        private PluginInitContext m_context;
+
+        public void Init(PluginInitContext context)
+        {
+            m_context = context;
+        }
+
+        public List<Result> Query(Query query)
+        {
+            var results = new List<Result>();
+            results.Add(new Result
+            {
+                Title = "Hello World",
+                SubTitle = "Sinple plugin test",
+                Action = c =>
+                {
+                    try
+                    {
+                        MessageBox.Show("Hello World !!");
+                        m_context.API.HideApp();
+                        return true;
+                    }
+                    catch (Exception e)
+                    {
+                        m_context.API.ShowMsg("Error", e.Message, null);
+                        return false;
+                    }
+                }
+            });
+            return results;
+        }
+    }
+}
